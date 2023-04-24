02865d03cabde5bd6cb971e7b1fc1b9b3a9a1fa6
Oskar Wiktor <oskar.wiktor3@wp.pl>
2023-04-24 13:37:41 +0200

    create-app-3

--- 01/src/App.js
+++ 01/src/App.js
@@ -4,14 +4,21 @@ function App() {
   return (
     <div className="container">
 
+      <div className="header">
+        <div className="Title"></div>
+        <div className="dark-mode"></div>
+      </div>
+
       <div className="expense-item-list-container">
         <ExpenseItem></ExpenseItem>
       </div>
+
       <div className="analist-container">
         <div className="add-new"></div>
         <div className="analist"></div>
         <div className="budget"></div>
       </div>
+
     </div>
   );
 }
