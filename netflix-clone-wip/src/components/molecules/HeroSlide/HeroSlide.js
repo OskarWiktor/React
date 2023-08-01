6393968be9a0d8c2abb9c8aea7b29946028c714a
Oskar Wiktor <oskar.wiktor3@wp.pl>
2023-08-01 14:06:40 +0200

    feat:Link on PlayButton and read more in HeroSlide

--- netflix-clone-wip/src/components/molecules/HeroSlide/HeroSlide.js
+++ netflix-clone-wip/src/components/molecules/HeroSlide/HeroSlide.js
@@ -1,19 +1,21 @@
 import LikeButton from "../../atoms/LikeButton/LikeButton";
 import PlayButton from "../../atoms/PlayButton/PlayButton";
+import {Link} from "react-router-dom";
 
 function HeroSlide(props) {
   const {title, genres, thumbnail, description} = props;
 
   const shortDescription = (description) => {
-    const maxLength = 280;
+    const maxLength = 200;
     if (description.length > maxLength) {
       return description.substring(0, maxLength) + "...";
     }
     return description;
   };
-  //ToDo 1. add read more, if description > 280, with button that show movie page with whole description, cast and so on
 
   const genresWithSpace = genres.join(" ");
+
+  const formattedTitle = title.toLowerCase().replace(/[^a-z0-9-]+/g, "-");
 
   return (
     <div className="hero-slide" style={{backgroundImage: `url(${thumbnail})`}}>
@@ -23,15 +25,24 @@ function HeroSlide(props) {
           <h2 className="hero-slide__text--title">{title}</h2>
           <p className="hero-slide__text--description">
             {shortDescription(description)}
+            <Link to={`/${formattedTitle}`}>
+              <p className="hero-slide__text--read-more">read more</p>
+            </Link>
           </p>
           <div className="hero-slide--icons">
-            <PlayButton>Play</PlayButton>
+            <Link to={`/${formattedTitle}`}>
+              <PlayButton>Play</PlayButton>
+            </Link>
             <LikeButton />
           </div>
         </div>
 
         <div className="hero-slide__thumbnail">
-          <img alt={`${title} movie`} className="hero-slide__thumbnail--img" src={thumbnail} />
+          <img
+            alt={`${title} movie`}
+            className="hero-slide__thumbnail--img"
+            src={thumbnail}
+          />
         </div>
       </div>
     </div>
