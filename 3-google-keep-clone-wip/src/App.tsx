1fbd4234357857128267ea49a5aa686fd1931310
Oskar Wiktor <oskar.wiktor3@wp.pl>
2023-08-29 16:26:10 +0200

    feat: OOP study

--- 3-google-keep-clone-wip/src/App.tsx
+++ 3-google-keep-clone-wip/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  class Animal {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+    walk() {
+      return `${this.name} is walking`;
+    }
+  }
+  class Human extends Animal {
+    genre: string;
+    constructor(name: string, age: number, genre: string) {
+      super(name, age);
+      this.genre = genre;
+    }
+    bio() {
+      return `My name is ${this.name} and I am ${this.age} old ${this.genre}`;
+    }
+    talk() {
+      return `${this.age} old ${this.name} is talking: `;
+    }
+  }
+
+  let Beata = new Human("Beata", 62, "woman");
+  
+  console.log(Beata, Beata.talk(), Beata.bio());
+
+  return <div className="App"></div>;
 }
 
 export default App;
